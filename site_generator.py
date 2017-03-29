ec6ef09d706a1aeb14cb27dbd329e54e0eb711e1
Mikhail Kuntsevich <moofspb@gmail.com>
2017-03-29 20:05:07 +0300

    Fix templates, site_generator and add generated site

--- site_generator.py
+++ site_generator.py
@@ -1,11 +1,14 @@
 import os
 import json
+import copy
 import markdown
 from jinja2 import Environment, FileSystemLoader
 
 
 SITE_DIR = 'site/'
 ARTICLE_DIR = 'articles/'
+MAIN_PAGE = 'index.html'
+CONFIG = 'config.json'
 
 
 def load_config(filepath):
@@ -33,7 +36,7 @@ def markdown_to_html(path_to_md):
 
 def get_html_article_path(md_path):
     article_path = os.path.splitext(md_path)[0]
-    return '{}{}.html'.format(SITE_DIR, article_path)
+    return '{}.html'.format(article_path)
 
 
 def create_template(template_path):
@@ -48,9 +51,10 @@ def create_template(template_path):
 
 
 def change_articles_paths(config):
-    for article in config['articles']:
+    html_config = copy.deepcopy(config)
+    for article in html_config['articles']:
         article['source'] = get_html_article_path(article['source'])
-    return config
+    return html_config
 
 
 def render_main_page(context, template_path='templates/main.html'):
@@ -58,12 +62,13 @@ def render_main_page(context, template_path='templates/main.html'):
     return template.render(config=context)
 
 
-def render_article_page(article, template_path='templates/article.html'):
+def render_article_page(article_info, template_path='templates/article.html'):
     template = create_template(template_path)
+    article_content = markdown_to_html(load_markdown(ARTICLE_DIR + article_info['source']))
     article_context = {
-        'title': article['title'],
-        'source': article['source'],
-        'content': markdown_to_html(load_markdown(article['source']))
+        'title': article_info['title'],
+        'source': article_info['source'],
+        'content': article_content
         }
     return template.render(config=article_context)
 
@@ -74,53 +79,13 @@ def write_generated_page(filepath, template):
 
 
 if __name__ == '__main__':
-    config_md_path = load_config('config.json')
+    config_md_path = load_config(CONFIG)
     config_html_path = change_articles_paths(config_md_path)
-    main_page_path = SITE_DIR + 'index.html'
+    main_page_path = SITE_DIR + MAIN_PAGE
     for article in config_html_path['articles']:
-        create_dir_for_html(article['source'])
+        create_dir_for_html(SITE_DIR + article['source'])
     create_dir_for_html(SITE_DIR)
     write_generated_page(main_page_path, render_main_page(config_html_path))
     for article in config_md_path['articles']:
-        write_generated_page(article['source'], render_article_page(article))
-
-    """
-    TODO:
-    1. render_article_page: fix get article content (line 66)
-    2. fix site/site in articles links in index.html
-    3. create function generate_site
-
-
-
-    main_template = 'templates/main.html'
-    article_template = 'templates/article.html'
-    config = load_config('config.json')
-    create_dir_for_html('site')
-    context = {
-        'topics': [topic for topic in config['topics']],
-        'articles': [article for article in config['articles']]
-    }
-    write_generated_page('site/index.html', (render_template(main_template, context)))
-    write_generated_page('site/')
-    # print(get_titles(load_config('config.json')))
-    topics = get_titles(load_config('config.json'))
-    articles = get_articles(load_config('config.json'))
-    html_from_md = markdown_to_html(load_markdown('articles/0_tutorial/8_cli.md'))
-    context = {
-        'topics': topics,
-        'articles': articles,
-        'article': html_from_md
-    }
-
-    create_dir_for_html('site')
-    # write_generated_page('site/index.html', (render_template('templates/main.html', context)))
-    # print(load_config('config.json'))
-
-    html_template = render_template('templates/article.html', context)
-
-    write_generated_page('site/8_cli.html', html_template)
-
-    # html_article = markdown_to_html(load_markdown('articles/0_tutorial/8_cli.md'))
-    # write_generated_page('site/8_cli.html', html_article)
-    """
-
+        article_dir = SITE_DIR + get_html_article_path(article['source'])
+        write_generated_page(article_dir, render_article_page(article))
